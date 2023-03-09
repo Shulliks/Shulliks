726c761409b784ab6f2e707dd1bc22b58753d468
Shulliks <127364268+Shulliks@users.noreply.github.com>
2023-03-09 00:12:41 -0300

    Repo
    Primera repo

---
+++ js_prueba.js
@@ -0,0 +1,7 @@
+
+
+const persona = '{"nombre": "Leonardo Da Vinci", "edad": 67, "nacionalidad": "italiano"}';
+const obj = JSON.parse(persona);
+console.log (obj);
+
+// Para mostrar que no me perdí de nada, aunque como "pidió" "Florencia", me quedé sin tiempo para hacerlo mejor...//
