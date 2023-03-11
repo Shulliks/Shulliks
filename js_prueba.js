2d6f092057b56e1d9f8de64d5db26bc34dcfa5ad
Shulliks <127364268+Shulliks@users.noreply.github.com>
2023-03-10 22:00:21 -0300

    ...
    .....

--- js_prueba.js
+++ js_prueba.js
@@ -4,4 +4,4 @@ const persona = '{"nombre": "Leonardo Da Vinci", "edad": 67, "nacionalidad": "it
 const obj = JSON.parse(persona);
 console.log (obj);
 
-// Para mostrar que no me perdí de nada, aunque como "pidió" "Florencia", me quedé sin tiempo para hacerlo mejor...//
+// Para mostrar que no me perdí de nada, aunque tal como lo "pidió" "LA Florencia", me quedé sin tiempo para hacerlo mejor...//
